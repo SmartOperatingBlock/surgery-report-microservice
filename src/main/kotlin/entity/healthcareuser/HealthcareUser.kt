a25980b311cae4f2728147b26fc520e65ccb1b32
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-09 17:33:14 +0200

    chore: add healthcare user concept

---
+++ src/main/kotlin/entity/healthcareuser/HealthcareUser.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.healthcareuser
+
+/**
+ * It represents a Healthcare user of the Healthcare national system.
+ * Each user is identified by their [taxCode] and has a [name] and a [surname].
+ */
+data class HealthcareUser(
+    val taxCode: TaxCode,
+    val name: String,
+    val surname: String,
+) {
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is HealthcareUser -> this.taxCode == other.taxCode
+        else -> false
+    }
+
+    override fun hashCode(): Int = this.taxCode.hashCode()
+}
+
+/**
+ * Tax Code of a [HealthcareUser].
+ * @param[value] the tax code.
+ */
+data class TaxCode(val value: String) {
+    init {
+        // Constructor validation: the code must not be empty
+        require(this.value.isNotEmpty())
+    }
+}
