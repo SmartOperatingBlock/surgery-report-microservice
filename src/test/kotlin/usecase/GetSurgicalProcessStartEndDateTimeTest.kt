953caf370259d74bbbf468b6fe473c25ea25bb5b
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-16 12:42:39 +0200

    test: add tests for use case needed to obtain start and end date time from a surgical process

---
+++ src/test/kotlin/usecase/GetSurgicalProcessStartEndDateTimeTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import usecase.SurgicalProcessData.simpleSurgicalProcess
+import usecase.SurgicalProcessData.simpleSurgicalProcessInterrupted
+import java.time.Instant
+
+class GetSurgicalProcessStartEndDateTimeTest : StringSpec({
+    val startDateTime = Instant.parse("2020-10-03T08:10:00Z")
+    val stopDateTimeTerminated = Instant.parse("2020-10-03T09:00:00Z")
+    val stopDateTimeInterrupted = Instant.parse("2020-10-03T08:25:00Z")
+
+    "It should be able to extract the start date time from a surgical process that is terminated correctly" {
+        val dateTime = GetSurgicalProcessStartEndUseCase(simpleSurgicalProcess).execute()
+        dateTime.first shouldBe startDateTime
+        dateTime.second shouldBe stopDateTimeTerminated
+    }
+
+    "It should be able to extract the start date time from a surgical process that has been interrupted" {
+        val dateTime = GetSurgicalProcessStartEndUseCase(simpleSurgicalProcessInterrupted).execute()
+        dateTime.first shouldBe startDateTime
+        dateTime.second shouldBe stopDateTimeInterrupted
+    }
+})
