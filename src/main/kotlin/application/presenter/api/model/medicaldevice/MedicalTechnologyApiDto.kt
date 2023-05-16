64efef14cc747a321947e6e077c749e1a97ce59f
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-16 17:36:15 +0200

    chore: add medical technology api dto

---
+++ src/main/kotlin/application/presenter/api/model/medicaldevice/MedicalTechnologyApiDto.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.api.model.medicaldevice
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Presenter class for the [entity.medicaldevice.MedicalTechnology] class.
+ * It returns: the [id], the [name], the [description], the [type] of the medical technology and moreover
+ * if it is [inUse].
+ */
+@Serializable
+data class MedicalTechnologyApiDto(
+    val id: String,
+    val name: String,
+    val description: String? = null,
+    val type: MedicalTechnologyApiDtoType,
+    val inUse: Boolean = false,
+)
+
+/**
+ * Presenter enum to handle medical technology type.
+ */
+@Serializable
+enum class MedicalTechnologyApiDtoType {
+    ENDOSCOPE,
+    XRAY,
+}
