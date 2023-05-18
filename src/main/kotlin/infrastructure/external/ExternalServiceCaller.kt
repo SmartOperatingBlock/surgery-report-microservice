daa3e6125420eb12d59e85d54a0d35b17825bcb3
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-18 16:14:35 +0200

    feat: implement room repository

--- src/main/kotlin/infrastructure/external/ExternalServiceCaller.kt
+++ src/main/kotlin/infrastructure/external/ExternalServiceCaller.kt
@@ -9,13 +9,16 @@
 package infrastructure.external
 
 import application.presenter.external.model.ApiResponses
+import application.presenter.external.model.BuildingManagementDtoModel
 import application.presenter.external.model.PatientManagementIntegrationDtoModel
 import application.presenter.external.model.StaffTrackingDtoModel
 import application.presenter.external.serialization.ExternalServiceSerialization.toHealthProfessionalTrackingInfo
 import application.presenter.external.serialization.ExternalServiceSerialization.toHealthcareUser
+import application.presenter.external.serialization.ExternalServiceSerialization.toRoomEnvironmentalData
 import entity.healthcareuser.HealthcareUser
 import entity.healthcareuser.TaxCode
 import entity.healthprofessional.HealthProfessionalID
+import entity.room.RoomEnvironmentalData
 import entity.room.RoomID
 import entity.tracking.TrackingInfo
 import io.ktor.client.HttpClient
@@ -28,18 +31,20 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.runBlocking
 import usecase.repository.HealthProfessionalRepository
 import usecase.repository.HealthcareUserRepository
+import usecase.repository.RoomRepository
 import java.time.Instant
 
 /**
  * The aim of this class is to provide the mean to access external microservices that have the data
  * needed by [HealthcareUserRepository].
  */
-class ExternalServiceCaller : HealthcareUserRepository, HealthProfessionalRepository {
+class ExternalServiceCaller : HealthcareUserRepository, HealthProfessionalRepository, RoomRepository {
     init {
         checkNotNull(System.getenv(PATIENT_MANAGEMENT_URL)) {
             "Patient Management Integration microservice url required"
         }
         checkNotNull(System.getenv(STAFF_TRACKING_URL)) { "Staff Tracking microservice url required" }
+        checkNotNull(System.getenv(BUILDING_MANAGEMENT_URL)) { "Building Management microservice url required" }
     }
 
     private val httpClient = HttpClient() {
@@ -48,45 +53,67 @@ class ExternalServiceCaller : HealthcareUserRepository, HealthProfessionalReposi
         }
     }
 
-    override fun getHealthcareUser(taxCode: TaxCode): HealthcareUser? =
-        runBlocking {
-            httpClient.get(System.getenv(PATIENT_MANAGEMENT_URL)) {
-                url {
-                    appendPathSegments("patients", taxCode.value)
-                }
-            }.let {
-                when (it.status) {
-                    HttpStatusCode.NotFound, HttpStatusCode.InternalServerError -> null
-                    else -> it.body<PatientManagementIntegrationDtoModel.HealthcareUserResultDto>().toHealthcareUser()
-                }
+    override fun getHealthcareUser(taxCode: TaxCode): HealthcareUser? = runBlocking {
+        httpClient.get(System.getenv(PATIENT_MANAGEMENT_URL)) {
+            url {
+                appendPathSegments("patients", taxCode.value)
+            }
+        }.let {
+            when (it.status) {
+                HttpStatusCode.NotFound, HttpStatusCode.InternalServerError -> null
+                else -> it.body<PatientManagementIntegrationDtoModel.HealthcareUserResultDto>().toHealthcareUser()
             }
         }
+    }
 
     override fun getHealthProfessionalTrackingInfo(
         roomID: RoomID,
         from: Instant,
         to: Instant,
-    ): List<TrackingInfo<HealthProfessionalID>> =
-        runBlocking {
-            httpClient.get(System.getenv(STAFF_TRACKING_URL)) {
-                url {
-                    appendPathSegments("rooms-tracking-data", roomID.value)
-                    parameters.append("from", from.toString())
-                    parameters.append("to", to.toString())
-                }
-            }.let { response ->
-                when (response.status) {
-                    HttpStatusCode.NoContent, HttpStatusCode.InternalServerError -> listOf()
-                    else ->
-                        response.body<ApiResponses.ResponseEntryList<StaffTrackingDtoModel.TrackingInfoResultDto>>()
-                            .entries
-                            .map { it.toHealthProfessionalTrackingInfo() }
-                }
+    ): List<TrackingInfo<HealthProfessionalID>> = runBlocking {
+        httpClient.get(System.getenv(STAFF_TRACKING_URL)) {
+            url {
+                appendPathSegments("rooms-tracking-data", roomID.value)
+                parameters.append("from", from.toString())
+                parameters.append("to", to.toString())
+            }
+        }.let { response ->
+            when (response.status) {
+                HttpStatusCode.NoContent, HttpStatusCode.InternalServerError -> listOf()
+                else ->
+                    response.body<ApiResponses.ResponseEntryList<StaffTrackingDtoModel.TrackingInfoResultDto>>()
+                        .entries
+                        .map { it.toHealthProfessionalTrackingInfo() }
             }
         }
+    }
+
+    override fun getRoomEnvironmentalData(
+        roomID: RoomID,
+        from: Instant,
+        to: Instant,
+    ): List<Pair<Instant, RoomEnvironmentalData>> = runBlocking {
+        httpClient.get(System.getenv(BUILDING_MANAGEMENT_URL)) {
+            url {
+                appendPathSegments("rooms", "data", roomID.value)
+                parameters.append("from", from.toString())
+                parameters.append("to", to.toString())
+            }
+        }.let { response ->
+            when (response.status) {
+                HttpStatusCode.NotFound, HttpStatusCode.InternalServerError -> listOf()
+                else ->
+                    response.body<ApiResponses.ResponseEntryList<
+                        ApiResponses.ResponseTimedEntry<BuildingManagementDtoModel.EnvironmentalDataApiDto>,>,>()
+                        .entries
+                        .map { Instant.parse(it.date) to it.entry.toRoomEnvironmentalData() }
+            }
+        }
+    }
 
     companion object {
         private const val PATIENT_MANAGEMENT_URL = "PATIENT_MANAGEMENT_INTEGRATION_MICROSERVICE_URL"
         private const val STAFF_TRACKING_URL = "STAFF_TRACKING_MICROSERVICE_URL"
+        private const val BUILDING_MANAGEMENT_URL = "BUILDING_MANAGEMENT_MICROSERVICE_URL"
     }
 }
