75b74272095c8679b639d4811909e4fa0aaf77e9
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-19 15:25:15 +0200

    chore: add mongo test utility

---
+++ src/test/kotlin/infrastructure/database/MongoTestUtility.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.database
+
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.mongo.transitions.Mongod
+import de.flapdoodle.os.CommonOS
+import de.flapdoodle.os.ImmutablePlatform
+import de.flapdoodle.os.Platform
+import de.flapdoodle.os.linux.LinuxDistribution
+import de.flapdoodle.os.linux.UbuntuVersion
+import de.flapdoodle.reverse.StateID
+import de.flapdoodle.reverse.transitions.Start
+import org.slf4j.LoggerFactory
+import de.flapdoodle.net.Net as Network
+
+private val logger by lazy { LoggerFactory.getLogger("TestsWithMongoDB") }
+
+private inline fun <reified T : Any> T.toTransition() = Start.to(StateID.of(T::class.java)).initializedWith(this)
+
+internal fun withMongo(operation: () -> Unit) {
+    if (System.getProperty("os.name").lowercase().contains("win")) {
+        logger.warn("Testing with MongoDB is disabled on Windows due to flaky behavior")
+    } else {
+        runCatching { Platform.detect(CommonOS.list()) }
+            .onFailure { logger.warn("Failed to detect platform", it) }
+            .onSuccess { detectedPlatform ->
+                val platform: Platform = detectedPlatform
+                    .takeUnless { it.operatingSystem() == CommonOS.Linux && it.distribution().isEmpty }
+                    ?: ImmutablePlatform.builder()
+                        .from(detectedPlatform)
+                        .distribution(LinuxDistribution.Ubuntu)
+                        .version(UbuntuVersion.Ubuntu_20_10)
+                        .build()
+                val mongod = Mongod.instance()
+                    .withNet(Net.of("localhost", 27017, Network.localhostIsIPv6()).toTransition())
+                    .withPlatform(platform.toTransition())
+                val runningState = mongod.start(Version.Main.V6_0)
+                try {
+                    operation()
+                } finally {
+                    runningState.current().stop()
+                }
+            }
+    }
+}
