f4f2715e3e0b1c70aca2d885317187c7a04ab228
Andrea Giulianelli <andrygiulio@gmail.com>
2023-05-19 18:22:14 +0200

    chore: add surgical process summary event

---
+++ src/main/kotlin/application/presenter/event/model/SurgicalProcessSummaryEvent.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Surgical process summary event.
+ */
+@Serializable
+data class SurgicalProcessSummaryEvent(
+    override val key: String,
+    override val data: SurgicalProcessSummaryPayload,
+    override val dateTime: String,
+) : Event<SurgicalProcessSummaryPayload>
